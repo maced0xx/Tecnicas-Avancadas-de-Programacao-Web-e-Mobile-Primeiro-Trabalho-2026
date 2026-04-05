2eb49044b96fe2f2669f30741da0912647306629
Anthony Macedo <anthonyfermacedo@gmail.com>
2026-04-05 20:59:47 -0300

    Delete segundo_trabalho_parcial directory

--- segundo_trabalho_parcial/test/segundo_trabalho_parcial_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:segundo_trabalho_parcial/segundo_trabalho_parcial.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
